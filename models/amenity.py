8f0005279c41ebd8b72d7959dcb5de36317c21c7
keimah507 <githinjibrian5@gmail.com>
2022-09-14 18:40:21 +0300

    Tasks 3 through 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel):
-    name = ""
+    """Features of the place to stay"""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship('Place',
+                                       secondary='place_amenity',
+                                       back_populates='amenities')
